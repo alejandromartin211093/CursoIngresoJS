723e619f53b14b7390c761d37a5a374e0296640e
alumno <alumno@utnfra.com.ar>
2020-02-04 12:32:52 -0300

    nuevo

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,13 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
-	alert("ok");
+	alert("esto funciona de maravilla")
 }
 
+{
+	var dato;
+    dato =	prompt("ingrese un dato");
+	
+	alert(dato);
+
+}
